c0bc52526617b857d6287c872b0b03f537bcd810
Sonali Ghanwat <sonalighanwat23@gmail.com>
2018-01-10 11:26:00 +0530

    <add user type component>

---
+++ src/app/components/login/login.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, URLSearchParams, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+import { LoginModel } from '../../models/login/login.model';
+@Injectable()
+export class LoginService {
+  loginUrl = "http://localhost:8085/HRMS/employee/login";
+
+  constructor(private http:Http) { }
+
+post(login: LoginModel):Observable<any> {
+	let Headers1 = new Headers({ 'Content-Type': 'application/json' });
+        let options = new RequestOptions({ headers: Headers1 });
+        return this.http.post(this.loginUrl, login,options)
+               .map(success => success)
+               .catch(this.handleError);
+               
+               
+    }
+
+    private handleError (error: Response | any) {
+	console.error(error.message || error);
+	return Observable.throw(error.status);
+    }
+
+}
---
+++ src/app/components/usertype/usertype.ts
@@ -0,0 +1,7 @@
+export class UserType{
+    public usertypeId: string; 
+    public usertypeName: string;
+    public description: string;
+    constructor()
+     {  }
+ } 
