dc4e64725d638879df32582f4c66833b1c2823c2
NishantNaikwade <nishant.naikwade@gmail.com>
2018-01-18 08:46:34 +0530

    Created base class for service and modified attendance service to use it and added ng2-translate to package.json

--- src/app/services/Attendance/attendance.service.ts
+++ src/app/services/Attendance/attendance.service.ts
@@ -4,20 +4,23 @@ import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
-import { Attendance} from '../../models/Attendance/Attendance.model';
+import { Attendance } from '../../models/Attendance/Attendance.model';
+import { BaseService } from '../base.service';
 @Injectable()
-export class AttendanceService {
+export class AttendanceService extends BaseService {
 
-  allAttendanceListUrl = "http://localhost:8085/HRMS/employeeattendance/list";
-  attendanceCreateUrl = "http://localhost:8085/HRMS/employeeattendance/create";
-  attendanceDeleteUrl = "http://localhost:8085/HRMS/employeeattendance/delete/";
-  attendancegetById ="http://localhost:8085/HRMS/employeeattendance/";
-  attendanceUpdateUrl = "http://localhost:8085/HRMS/employeeattendance/update"
-  
-  constructor(private http: Http) { }
-  
+  allAttendanceListUrl = "employeeattendance/list";
+  attendanceCreateUrl = "employeeattendance/create";
+  attendanceDeleteUrl = "employeeattendance/delete/";
+  attendancegetById = "employeeattendance/";
+  attendanceUpdateUrl = "employeeattendance/update"
+
+  constructor(protected http: Http) {
+    super(http);
+  }
+
   getAllAttendance(): Observable<Attendance[]> {
-    return this.http.get(this.allAttendanceListUrl)
+    return this.http.get(this.buidURL(this.allAttendanceListUrl))
       .map(this.extractData)
       .catch(this.handleError);
   }
@@ -25,41 +28,33 @@ export class AttendanceService {
   createEmployeeAttendance(attendance: Attendance): Observable<number> {
     let cpHeaders = new Headers({ 'Content-Type': 'application/json' });
     let options = new RequestOptions({ headers: cpHeaders });
-    return this.http.post(this.attendanceCreateUrl, attendance, options)
+    return this.http.post(this.buidURL(this.attendanceCreateUrl), attendance, options)
       .map(success => success.status)
       .catch(this.handleError);
   }
 
   deleteEmployeeAttendanceById(id: string): Observable<number> {
     let cpHeaders = new Headers({ 'Content-Type': 'application/json' });
-    return this.http.delete(this.attendanceDeleteUrl+id)
+    return this.http.delete(this.buidURL(this.attendanceDeleteUrl + id))
       .map(success => success.status)
       .catch(this.handleError);
   }
   getEmployeeAttendanceById(id: string): Observable<Attendance> {
     let cpHeaders = new Headers({ 'Content-Type': 'application/json' });
     let cpParams = new URLSearchParams();
-    cpParams.set('id', id);			
+    cpParams.set('id', id);
     let options = new RequestOptions({ headers: cpHeaders, params: cpParams });
-    return this.http.get(this.attendancegetById+id)
+    return this.http.get(this.buidURL(this.attendancegetById + id))
       .map(this.extractData)
       .catch(this.handleError);
-      }	
+  }
 
-  updateEmployeeAttendance(attendance: Attendance):Observable<number> {
+  updateEmployeeAttendance(attendance: Attendance): Observable<number> {
     let cpHeaders = new Headers({ 'Content-Type': 'application/json' });
-          let options = new RequestOptions({ headers: cpHeaders });
-          return this.http.put(this.attendanceUpdateUrl, attendance, options)
-                 .map(success => success.status)
-                 .catch(this.handleError);
-      }
+    let options = new RequestOptions({ headers: cpHeaders });
+    return this.http.put(this.attendanceUpdateUrl, attendance, options)
+      .map(success => success.status)
+      .catch(this.handleError);
+  }
 
-  private extractData(res: Response) {
-    let body = res.json();
-    return body;
-  }
-  private handleError(error: Response | any) {
-    console.error(error.message || error);
-    return Observable.throw(error.status);
-  }
 }
---
+++ src/app/services/base.service.ts
@@ -0,0 +1,29 @@
+import { Observable } from 'rxjs/Observable';
+import { Http, Response, Headers, URLSearchParams, RequestOptions } from '@angular/http';
+
+export class BaseService {
+
+    protected base_url = "http://localhost:8085/HRMS/";
+
+//   allAttendanceListUrl = "http://localhost:8085/HRMS/employeeattendance/list";
+//   attendanceCreateUrl = "http://localhost:8085/HRMS/employeeattendance/create";
+//   attendanceDeleteUrl = "http://localhost:8085/HRMS/employeeattendance/delete/";
+//   attendancegetById ="http://localhost:8085/HRMS/employeeattendance/";
+//   attendanceUpdateUrl = "http://localhost:8085/HRMS/employeeattendance/update"
+  
+  constructor(protected http: Http) { }
+
+  protected buidURL(endpoint: string): string{
+    return this.base_url + endpoint;
+  }
+
+  protected extractData(res: Response) {
+    let body = res.json();
+    return body;
+  }
+  protected handleError(error: Response | any) {
+    console.error(error.message || error);
+    return Observable.throw(error.status);
+  }
+
+}
